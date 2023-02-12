93d0388e97e9bc80a6839635b0d0ff7e42249c75
DavidAlbUwU <117684736+DavidAlbUwU@users.noreply.github.com>
2023-02-12 14:51:19 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -4,10 +4,10 @@ menuListArray = ["Pizza Vegetariana","Pizza de Frango","Pizza Portuguesa","Pizza
 function getMenu(){
     var htmldata="";
     menuListArray.sort();
-    for(var i=0;i<menulistArray.legth;i++){
+    for(var i=0;i<menulistArray.Legth;i++){
         htmldata=htmldata+ menuListArray[i] + '<br>'
     }
-    document.getElementById("displayAddedMenu").innerHTML = htmldata;
+    document.getElementById("displayMenu").innerHTML = htmldata;
 }
 
 function addItem(){
@@ -16,10 +16,10 @@ function addItem(){
     var item=document.getElementById("addItem").value;
     menuListArray.sort();
     htmldata=""
-    for(var i=0;i<menuListArray.legth;i++){
+    for(var i=0;i<menuListArray.Legth;i++){
         htmldata=htmldata+imgtags+ menuListArray[i]+'<br>'
     }
-    document.getElementById("dispalyAddedMenu").innerHTML= htmldata;
+    document.getElementById("displayAddedMenu").innerHTML= htmldata;
 }
 
 function addTop(){
